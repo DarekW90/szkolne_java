779f5719d030d0702db00d65b080113529e700a3
Dariusz Walda <walda.darek@gmail.com>
2023-11-12 21:22:19 +0100

    task 1 and 2

--- Zadania I/Zadanie 02/src/Main.java
+++ Zadania I/Zadanie 02/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Podaj liczbę godzin: [zatwierdź enterem]");
+            float godziny = scan.nextFloat();
+            float minuty = godziny * 60;
+
+            System.out.println("Podaj liczbę minut: [zatwierdź enterem]");
+            float minuty2 = scan.nextFloat();
+            float godziny2 = minuty2 / 60;
+
+            System.out.println(godziny + " godziny, to: " + minuty + " minut.");
+            System.out.println(minuty2 + " minuty, to: " + godziny2 + " godzin.");
+        }
     }
 }
