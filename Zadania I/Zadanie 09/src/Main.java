e3e7cb94ed0d16448b97e32fb731494f7dc07d50
Dariusz Walda <walda.darek@gmail.com>
2023-11-14 21:49:31 +0100

    task 9 completed

--- Zadania I/Zadanie 09/src/Main.java
+++ Zadania I/Zadanie 09/src/Main.java
@@ -1,5 +1,63 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        char repeatProgram;
+        char selectOption;
+        do {
+            System.out.println("Podaj liczbę (0 - 9):");
+            int number = scan.nextInt();
+
+            switch (number) {
+                case 0:
+                    System.out.println("Wartość to zero");
+                    break;
+                case 1:
+                    System.out.println("Wartość to jeden");
+                    break;
+                case 2:
+                    System.out.println("Wartość to dwa");
+                    break;
+                case 3:
+                    System.out.println("Wartość to trzy");
+                    break;
+                case 4:
+                    System.out.println("Wartość to cztery");
+                    break;
+                case 5:
+                    System.out.println("Wartość to pięć");
+                    break;
+                case 6:
+                    System.out.println("Wartość to sześć");
+                    break;
+                case 7:
+                    System.out.println("Wartość to siedem");
+                    break;
+                case 8:
+                    System.out.println("Wartość to osiem");
+                    break;
+                case 9:
+                    System.out.println("Wartość to dziewięć");
+                    break;
+                default:
+                    System.out.println("Podano wartość z poza zakresu 0-9 ");
+                    break;
+            }
+
+
+            System.out.println();
+            System.out.print("Czy wykonać program jeszcze raz? T/N: ");
+            repeatProgram = scan.next().charAt(0);
+
+        }
+        while (repeatProgram == 'T' || repeatProgram == 't') ;
+
+        System.out.println("Koniec programu.");
+        scan.close();
+
     }
 }
+
